60e6a857e71245024d3e6bba5b392589eeaf54fb
Nigel Lowry <nigel@lemmata.co.uk>
2011-05-07 19:58:19 +0100

    Added skeleton interface of image registration. The interface is probably a bit wrong and should two take images rather than files.

---
+++ src/main/java/uk/co/lemmata/image/oc/OrientationCorrelationImageAligner.java
@@ -0,0 +1,14 @@
+package uk.co.lemmata.image.oc;
+
+import java.awt.geom.AffineTransform;
+import java.io.File;
+
+public class OrientationCorrelationImageAligner implements ImageAligner {
+
+	@Override
+	public AffineTransform multiModal(File imageFile1, File imageFile2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
