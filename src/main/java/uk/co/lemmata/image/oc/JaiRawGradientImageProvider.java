fbad4905403c21887a4e30ecec61f6ab01c5c581
Nigel Lowry <nigel@lemmata.co.uk>
2011-05-17 23:25:19 +0100

    Got some code working but this is hard to test.

--- src/main/java/uk/co/lemmata/image/oc/JaiRawGradientImageProvider.java
+++ src/main/java/uk/co/lemmata/image/oc/JaiRawGradientImageProvider.java
@@ -1,13 +1,27 @@
 package uk.co.lemmata.image.oc;
 
+import java.awt.image.renderable.*;
+
 import javax.media.jai.*;
 
 public class JaiRawGradientImageProvider implements RawGradientImageProvider {
+	
+	private static final float[] kernelData = {-1.0f, 0.0f, 1.0f};
+	private static final KernelJAI dx = new KernelJAI(3, 1, kernelData);
+	private static final KernelJAI dy = new KernelJAI(1, 3, kernelData);
 
 	@Override
 	public RenderedOp getRawGradient(RenderedOp renderedOp) {
-		// TODO Auto-generated method stub
-		return null;
+		final RenderedOp x = JAI.create("convolve", renderedOp, dx);
+		final RenderedOp y = JAI.create("convolve", renderedOp, dy);
+		
+		// TODO not sure what this does with edge pixels
+		
+		final ParameterBlock pb = new ParameterBlock();
+		pb.addSource(x);
+		pb.addSource(y);
+		
+		return JAI.create("bandmerge", pb);
 	}
 
 }
--- src/test/java/uk/co/lemmata/image/oc/RawGradientImageProviderTest.java
+++ src/test/java/uk/co/lemmata/image/oc/RawGradientImageProviderTest.java
@@ -3,6 +3,8 @@ package uk.co.lemmata.image.oc;
 import static org.hamcrest.core.Is.*;
 import static org.junit.Assert.*;
 
+import java.io.*;
+
 import javax.media.jai.*;
 
 import org.junit.*;
@@ -16,6 +18,7 @@ public class RawGradientImageProviderTest {
 	@Before
 	public void setUp() throws Exception {
 		provider = new JaiRawGradientImageProvider();
+		renderedOp = new JaiGrayscaleProvider().toGrayscale(new File("src/test/resources/colour.jpg"));
 	}
 	
 	@Test
@@ -23,6 +26,14 @@ public class RawGradientImageProviderTest {
 		final RenderedOp complexImage = provider.getRawGradient(renderedOp);
 		
 		assertThat(complexImage.getNumBands(), is(2));
+	}
+	
+	@Test
+	public void convolvedImageHasSameDimensions() throws Exception {
+		final RenderedOp complexImage = provider.getRawGradient(renderedOp);
+		
+		assertThat(complexImage.getWidth(), is(renderedOp.getWidth()));
+		assertThat(complexImage.getHeight(), is(renderedOp.getHeight()));
 	}
 
 }
